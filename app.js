62ca9cf9317cf3639af421c400cebf97045c0b47
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-09-02 11:28:31 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Welcome to CI Pipeline Automator!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
